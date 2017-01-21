3127b807187ec5925aa85d366a793e37a9b6b6c3
Michael <migamiga@web.de>
2017-01-21 17:40:31 +0100

    init

---
+++ app/models/library.js
@@ -0,0 +1,39 @@
+exports.definition = {
+    config: {
+        columns: {
+            "id": "INTEGER PRIMARY KEY AUTOINCREMENT",
+            "title": "text",
+            "subtitle": "text",
+            "image": "text"
+        },
+        adapter: {
+            type: "sql",
+            collection_name: "library",
+            idAttribute: "id",
+        }
+    },
+    extendModel: function(Model) {
+        _.extend(Model.prototype, {
+            // extended functions and properties go here
+            validate: function() {
+                // check if a book is valid: title, subtitle and image are not empty
+                var val = false;
+                if (this.get("title") != null && this.get("subtitle") != null && this.get("image") != null) {
+                    return true;
+                } else {
+                    return false;
+                }
+                return false;
+            }
+        });
+
+        return Model;
+    },
+    extendCollection: function(Collection) {
+        _.extend(Collection.prototype, {
+            // extended functions and properties go here
+        });
+
+        return Collection;
+    }
+};
