8cc1490227cbe6752d6532bef0eec8f4b7c48def
m1ga <migamiga@web.de>
2025-01-11 18:29:48 +0100

    update

--- app/models/library.js
+++ app/models/library.js
@@ -15,7 +15,6 @@ exports.definition = {
 	extendModel: function(Model) {
 		_.extend(Model.prototype, {
 			// extended functions and properties go here
-<<<<<<< HEAD
 
 			transform: function() {
 				var model = this;
@@ -47,36 +46,6 @@ exports.definition = {
 		_.extend(Collection.prototype, {
 			// extended functions and properties go here
 		});
-
-=======
-			validate: function() {
-				// check if a book is valid: title, subtitle and image are not empty
-				var val = false;
-				if (this.get("title") != null && this.get("subtitle") != null && this.get("image") != null) {
-					return true;
-				} else {
-					return false;
-				}
-				return false;
-			}
-		});
- 
-		return Model;
-	},
-	extendCollection: function(Collection) {
-		_.extend(Collection.prototype, {
-			// extended functions and properties go here
-			dump: function() {
-				// get all models
-				return this.models;
-			},
-			clear: function() {
-				// remove/destroy all models
-				_.invoke(this.toArray(), 'destroy');
-			}
-		});
-
->>>>>>> 01bd909b06b5e5edcac822b22dc3afaf3284fe79
 		return Collection;
 	}
 };
